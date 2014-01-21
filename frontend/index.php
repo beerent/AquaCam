de606d790caa1e251994903d7b5d5474cb7e4de9
Brent Ryczak <brentryczak@gmail.com>
2014-01-20 23:46:14 -0500

    another update. will conclude in one of the next few.

--- frontend/index.php
+++ frontend/index.php
@@ -1,33 +1,44 @@
 <html>
   <style> 
     h1{ font-size:72; text-align: center; }
-    #banner{ background-image:  <a href="myfile.htm"><img src="IMG-5707.jpeg"></a>; }
+    
+    #banner{ background-image:  <a href="myfile.htm">
+      <img src="IMG-5707.jpeg"></a>; 
+    }
+
+    #image{
+      width:100%;
+      height = 600 px;
+      font-size = 72;
+      float:right;
+    }
+
   </style>
   <body>
     <div id = "banner">
       <h1>AQUARICAM!</h1>
     </div>
-    <?php
-      echo "<center>Please select an Aquarium to view:</center>";
+    <center> Please select an Aquarium to view:</center>
 
-    ?>
     <!-- The form Value's represent the user. There are TWO users,
          Riley (0), and Brent(1).
       -->
+    Riley's Fish Tank
     <form action="user.php" 
 	  method="post">
       <input type="image" 
-	     src="IMG_5733.jpeg"
-	     height="42"
-	     width="82"
+	     src="IMG_5733.jpeg";
+	     width="20%"
       />
       <input type="hidden" name="Language" value="0">
     </form>
-
+    
+    Brent's Turtle Tank
     <form action="user.php" 
 	  method="post">
       <input type="image" 
-	     src="IMG_5733.jpeg" 
+	     src="IMG_5733.jpeg";
+	     width="20%"
       />
       <input type="hidden" name="Language" value="1">
     </form>
--- frontend/usertest.php
+++ frontend/usertest.php
@@ -1,11 +1,6 @@
 <html>
   <body>
     <?php
-      $brentIP = "192.168.1.141";
-      $brentPORT = 5678;
-
-      $rileyIP = "192.168.1.142";
-      $rileyPORT  = 5678;   
       
       function getServerIP(){
 
