04fc88059f38d19b0c5d5d6e79cf55ba36b21841
Brent Ryczak <brentryczak@gmail.com>
2014-01-22 14:19:06 -0500

    added a time clock for the project.

---
+++ backend/server/timeclock/logger.py
@@ -0,0 +1,24 @@
+from time import gmtime, strftime
+import sys
+
+if len(sys.argv) != 3:
+    print "please enter your name!"
+    exit(1)
+
+str = sys.argv[1]
+print str
+if str != "riley" and str != "brent":
+    print str + " is not riley"
+    print "invalid user, goodbye"
+    exit(1)
+
+if sys.argv[2] != "in" and sys.argv[2] != "out":
+    "wrong in or out input."
+    exit(1)
+
+str += " " + sys.argv[2] + ": " + strftime("%Y-%m-%d %H:%M:%S")   
+
+with open("clock.txt", "a") as clock:
+    clock.write(str);
+
+print"logged " + str
