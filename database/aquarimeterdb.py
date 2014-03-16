00189fff98802739c91412085aff9a56b9588eb2
Brent Ryczak <brentryczak@gmail.com>
2014-03-16 18:07:02 -0400

    working on sevrer

--- database/aquarimeterdb.py
+++ database/aquarimeterdb.py
@@ -1,10 +1,15 @@
 import MySQLdb
+import socket
+
+#server setup
+serverSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serverSock.bind((socket.gethostname(), 6666))
 
 #global var of the database
 host = 'localhost';
 user = 'root'
-password = 'arcland'
-database_name = 'aquarameter'
+password = 'cuse1234'
+database_name = 'aquarimeter'
 
 database = MySQLdb.connect(host, user, password, database_name)
 
@@ -34,10 +39,13 @@ def menu():
         print("enter command")
         execute(raw_input())
 
+def runServer():
+	serverSock.listen(5)
+	print("wainting for connection...")
+	while 1:
+		(clientSock, address) = serverSock.accept()
+		ct = client_thread(clientSock)
+		print("connection made.")
+
 #menu()
-
-
-
-
-
-
+runServer()
