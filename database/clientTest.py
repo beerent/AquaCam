181984cd35c3d40dfeaa6a1ca9ce56c9cc8edcd1
Brent Ryczak <brentryczak@gmail.com>
2014-03-20 16:54:46 -0400

    working on comments

--- database/clientTest.py
+++ database/clientTest.py
@@ -8,4 +8,7 @@ s.send("update aquarium active N aquarium_name test")
 data = s.recv(512)
 print(data)
 
-#insert: table, 
+#insert: insert 
+
+#update: table value_to_change desired_change record_to_change actual_record_value
+   # ex: update aquarium active N aquarium_name riley1
